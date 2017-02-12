f8b674fde46c83a3d3653417b1877e11b14c8cc1
Felipe Aguirre Martinez <faguirre@workit.fr>
2017-02-12 22:32:40 +0100

    Refactor. Fit predictor before if name == main

--- predictor.py
+++ predictor.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestClassifier
 from transformers import PassengerTransformer
+
+
+train = pd.read_csv('data/cleandata.train.csv')
+test = pd.read_csv('data/cleandata.test.csv')
 
 
 predictor = make_pipeline(
     PassengerTransformer,
     RandomForestClassifier(random_state=123)
 )
+predictor.fit(train, train.survived)
+
 
 if __name__ == "__main__":
-    import pandas as pd
 
-    train = pd.read_csv('data/cleandata.train.csv')
-    test = pd.read_csv('data/cleandata.test.csv')
-
-    predictor.fit(train, train.survived)
     score = predictor.score(test, test.survived)
     print("Score: {:0.4f}".format(score))
+
