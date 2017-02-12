4faf251affd5f7668c595c5a0211ebe3db8e6498
Felipe Aguirre Martinez <faguirre@workit.fr>
2017-02-12 23:03:38 +0100

    Serialize prediction with ID and probability

--- predictor.py
+++ predictor.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import pandas as pd
+
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestClassifier
 from transformers import PassengerTransformer
@@ -18,8 +20,19 @@ predictor = make_pipeline(
 predictor.fit(train, train.survived)
 
 
+def serialized_prediction(df):
+
+    response = pd.DataFrame(np.vstack([predictor.predict(df),
+                                       predictor.predict_proba(df).max(axis=1)]).T,
+                            columns=["Survived", "Probability"],
+                            index=df['passengerid'])
+
+    return response.to_json(orient='index')
+
 if __name__ == "__main__":
 
     score = predictor.score(test, test.survived)
     print("Score: {:0.4f}".format(score))
 
+    random_prediction = serialized_prediction(test.sample(1))
+    print("Prediction: {0!r}".format(random_prediction))
